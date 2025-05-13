1b0d1442bbbadd0fc9361729c6e4261bd717ffa5
PLM-18 <philemonmuleya@gmail.com>
2025-05-13 23:29:04 +0200

    chore: update dependencies and add ESLint configuration
    - Added @nuxt/eslint, axios, and eslint to package.json
    - Created Header.vue component file
    - Added ESLint configuration file
    - Created Home.vue page file

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  runtimeConfig: {
+    strapiApiToken: process.env.STRAPI_API_TOKEN,
+    strapiApiUrl: process.env.STRAPI_API_URL, 
+    public: {
+      // Any variables here will be exposed to the client-side (not recommended for the API token)
+    }
+  },
+
+  modules: ['@nuxt/eslint'],
 })
